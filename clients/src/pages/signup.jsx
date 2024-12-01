b662904fa66cc03d7eee675d5e7c44a17dd0e2cd
Ali-Peter <aliogochukwu06@gmail.com>
2024-12-01 04:23:36 +0100

    Update signup.jsx

--- clients/src/pages/signup.jsx
+++ clients/src/pages/signup.jsx
@@ -40,7 +40,7 @@ const SignUp = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/api/register", {
+      const response = await fetch("https://basic-full-stack-project-backend.onrender.com/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
